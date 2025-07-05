02225e54dd192cf409d8fd35856abc160c2a3633
denriq55 <142200008+denriq55@users.noreply.github.com>
2025-07-05 15:08:39 -0700

    validate and assign form inputs

--- create_form.php
+++ create_form.php
@@ -1,29 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Secure Product Contact Form</title>
 </head>
-<body>
-    <form action="create_form.php" method="POST">
-        <label for="fullName">Full name:</label>
-        <input type="text" id="fullName" name="fullName" required><br>
+<form action="create_form.php" method="POST">
+    <label for="fullName">Full name:</label>
+    <input type="text" id="fullName" name="fullName" required><br>
 
-        <label for="email">Email address:</label>
-        <input type="text" id="email" name="email" required><br>
+    <label for="email">Email address:</label>
+    <input type="text" id="email" name="email" required><br>
 
-        <label for="topic">Topic:</label>
-        <input type="text" id="topic" name="topic" required><br>
+    <label for="topic">Topic:</label>
+    <input type="text" id="topic" name="topic" required><br>
 
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea><br>
+    <label for="message">Message:</label>
+    <textarea id="message" name="message" required></textarea><br>
 
-        <input type="submit" value="Submit Form">
-    </form>
+    <input type="submit" value="Submit Form">
+</form>
 
+<?php
+//Function to validate input
+function validateInput($data, $fieldName) {
+    global $errorCount;
 
+    //if field is blank, display 'required' message and increment $errorCount
+    //otherwise clean it
+    if (empty($data)) {
+        echo "\"$fieldName\" is a required field.</br>\n";
+        ++$errorCount;
+        $userInput = "";
 
-    
-</body>
-</html>
+    } else {
+        $userInput = trim(stripslashes(($data)));
+    }
+    return($userInput);
+}
+
+//Function to validate email 
+function validateEmail($data, $fieldName) {
+    global $errorCount; 
+
+    if(empty($data)) {
+        echo "\"$fieldName\" is a required field.</br>\n";
+        ++$errorCount;
+        $userInput = "";
+    } else {
+        $userInput = filter_var($data, FILTER_SANITIZE_EMAIL);
+
+        if (!filter_var($userInput, FILTER_VALIDATE_EMAIL)) {
+            echo "\"$fieldName\" is not a valid email address.</br>\n";
+            ++$errorCount;
+        }
+        return($userInput);
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$name = validateInput($_POST['fullName'], "Your name");
+$email = validateEmail($_POST['email'], "Your Email");
+$topic = validateInput($_POST['topic'], "Topic");
+$message = validateInput($_POST['message'], "Message");
+
+}
+
+?>
